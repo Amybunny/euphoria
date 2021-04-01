d433b8cbbf3e9566d046de0c392fce4531bf134f
Amubunny <febunny48@gmail.com>
2021-04-01 16:53:36 +0900

    frontpage updated

--- footer.php
+++ footer.php
@@ -1,4 +1,10 @@
 </div>
+  <section class="wrap back-white">
+    <h1 class="text-center contact-title">お問い合わせ</h1>
+    <p class="text-center">TEL：080-0000-0000</p>
+    <p class="text-center">メール：euphoria@gmail.com</p>
+    <a class="line-btn" href=""><img src="/wordpress/wp-content/themes/ayaka/line.png" alt=""></a>
+  </section>
   <footer class="footer">
 
     <?php if(has_nav_menu('menu-2')): ?>
--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-<img class="top-img" src="/wordpress/wp-content/themes/ayaka/top.png" alt="">
+<img class="top-img" src="/wordpress/wp-content/themes/ayaka/youDSC_0057のコピー.jpg" alt="">
 
-<section class="wrap">
+<section class="wrap back-white">
   <div class="container">
     <h1 class="text-center front-title">レイキは日本発祥の<br>「手当て療法」です！
     </h1>
@@ -11,7 +11,7 @@
     レイキヒーリングは、ヒーラーが手を当てることによって気のバランスを調節し、癒やしを与える技法です。
     </p>
 
-    <img style="width:60%;" src="/wordpress/wp-content/themes/ayaka/cat1.jpeg" alt="">
+    <img src="/wordpress/wp-content/themes/ayaka/cat1.jpeg" alt="">
     <p class="text-center">実際の施療の様子</p>
 
     <h1 class="text-center front-title">レイキの主なメリット5つ</h1>
@@ -32,11 +32,27 @@
     ぼく自身がパニック障害、抗不安薬(ベンゾジアゼピン系)の重篤な副作用をレイキで克服したのもあり、当院は精神面での体調が優れない方、精神薬の副作用が強く出すぎてつらい方へのヒーリングが最も得意です。
     </p>
 
-    <a class="more-btn more-btn-yellow" href="http://localhost:8888/wordpress/aboutreiki/">その他の詳しい効果など</a>
+    <a class="more-btn back-yellow" href="http://localhost:8888/wordpress/aboutreiki/">その他の詳しい効果など</a>
+  </div>
+</section>
+
+<section class="wrap back-yellow">
+  <div class="container">
+    <h1 class="text-center front-title">プロフィール</h1>
+    <img src="/wordpress/wp-content/themes/ayaka/profile.jpg" alt="">
+    <p class="text-center">レイキヒーラー</p>
+    <p class="text-center">飯塚優亮</p>
+    <br>
+    <p>18歳の時に発症し、10年間苦しんだパニック障害をレイキで克服。
+    丸4年間服用していた「ベンゾジアゼピン系抗不安薬」による激しい副作用もあり、生き地獄を味わっていたが、これもレイキのヒーリングパワーに助けられ、完全断薬に成功。
+    苦しんでいる人の助けになるべく、現在は福岡市を拠点にレイキヒーラーとして活動している。
+    </p>
+
+    <a href="http://localhost:8888/wordpress/profile/" class="more-btn back-white">より詳しいプロフィールへ</a>
   </div>
 </section>
   
-<section>
+<section class="wrap back-white">
   <?php
     $information= get_posts( array(
     //ここで取得条件を指定する
@@ -72,7 +88,77 @@
       ?>
     </ul>
   <?php endif; ?>
-  <a class="more-btn more-btn-yellow" href="http://localhost:8888/wordpress/new/">もっと見る</a>
+  <a class="more-btn back-yellow" href="http://localhost:8888/wordpress/new/">ブログ一覧へ</a>
+</section>
+
+<section class="wrap back-yellow">
+  <div class="container">  
+    <h1 class="text-center front-title">アクセスガイド</h1>
+    <p>当院は、決まった店舗はございません。<br>
+    ・福岡市中心部のレンタルサロン<br>
+    ・自宅訪問施療<br>
+    にて施術をしております。<br>
+    <br>
+    （レンタルサロンのご案内）<br>
+    福岡市中央区の薬院駅付近に2箇所、天神駅付近に1箇所ございます。<br>
+    以下に詳しいアクセスを記載いたします。お客様のご都合に合わせ、場所をお選びください。
+    </p>
+    <a href="http://localhost:8888/wordpress/access/" class="more-btn back-white">詳しいアクセス</a>
+  </div>
+</section>
+
+<section class="wrap back-white">
+  <div class="container">
+    <h1 class="text-center front-title">よくある質問</h1>
+    <div class="qa-box">
+      <h2 class="question fw6">Q .ヒーリングを受けている間はどうすればいいのですか？</h2>
+      <p>A .エネルギーを受け取ると眠くなる場合がございますので、運転などは控えてください。
+      家でリラックスした状態で受けていただくのが理想です。スマホや読書、入浴などはしていただいていてかまいません。
+      </p>
+    </div>
+    <div class="qa-box">
+      <h2 class="question fw6">Q .オプションありとなしでは差がありますか？</h2>
+      <p>A .通常500円のヒーリングでもエネルギーが十分流せますのでご安心ください。
+      ただし、より深いレベルでのヒーリングをご希望の方は、オプションの【スピリットレイキ】追加をおすすめします。イエスやケルト神アニュといった、神様系の波動の高いエネルギーです。
+      </p>
+    </div>
+    <div class="qa-box">
+      <h2 class="question fw6">Q .健康上の問題はないのですが、人間関係が最悪で…。それもなんとかできるのでしょうか？</h2>
+      <p>A .人間関係そのものをレイキすることが可能です。
+      このように、生き物でなくても癒やしのエネルギーを届けられるのが海外系レイキの素晴らしいところです。
+      本当に何でもレイキできますので、お気軽にご相談ください。
+      </p>
+    </div>
+    <a class="more-btn back-yellow" href="http://localhost:8888/wordpress/voice/">もっと見る</a>
+  </div>
+</section>
+
+<section class="wrap back-yellow pb32">
+  <div class="container">  
+    <h1 class="text-center front-title">料金一覧</h1>
+    <p>・5000円<br>
+    ・初回限定4000円<br>
+    （まずは一度お試しください）<br>
+    <br>
+    施療の時間は約40分〜50分程度です。簡単なカウンセリング含め、トータル1時間程度になります。<br>
+    <br>
+    ・遠隔ヒーリング2500円<br>
+    （遠隔について、詳しくは<a class="remote-link" href="http://localhost:8888/wordpress/remote/">こちら</a>から）</p>
+  </div>
+</section>
+
+<section class="wrap back-white">
+  <div class="container">
+    <h1 class="text-center front-title">お客様の声</h1>
+    <p class="voice back-yellow">とても良かったです。ヒーリング中の体感も強かったですが翌日のスッキリ感がとてもありました♪またよろしくお願い致します。</p>
+    <p class="voice back-yellow ">月1,2回猫ちゃんにレイキを送ってもらってます。調子悪いなって時にお願いして、30分から半日くらいで良くなるのでいつも助かってます。</p>
+    <p class="voice back-yellow">肋間神経痛で動けなかったのですが、やって頂いた30分後から痛みが和らぎ始めました！助かりました、ありがとうございました。</p>
+    <p class="voice back-yellow">迅速にヒーリングしてくださりありがとうございました。
+    ヒーリングもとても強力で浄化の効果が感じられましたし不調な部分も当たってました。
+    メッセージも今の自分の悩みにピッタリな言葉でした。
+    またぜひよろしくお願いします。</p>
+    <a class="more-btn back-yellow" href="http://localhost:8888/wordpress/voice/">もっと見る</a>
+  </div>
 </section>
 
 <?php 
